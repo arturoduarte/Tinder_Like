74434fcf2e58b570706de441504e3f126f607f2c
arturo <informa.fj@gmail.com>
2018-02-23 16:55:57 -0300

    Primer commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+	protect_from_forgery with: :exception
+	before_action :configure_permitted_parameters, if: :devise_controller?
+	
+	private
+	def filter_admin!
+		authenticate_user!
+		redirect_to root_path, alert: "No tienes acceso" unless current_user.admin?
+	end
+	
+	def configure_permitted_parameters
+		devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :username, :admin])
+	end
 end
---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+	has_many :histories
+	# Include default devise modules. Others available are:
+	# :confirmable, :lockable, :timeoutable and :omniauthable
+	devise :database_authenticatable, :registerable,
+	:recoverable, :rememberable, :trackable, :validatable
+	
+	validates :name, presence: true		 
+	validates :username, presence: true, uniqueness: true
+
+end
